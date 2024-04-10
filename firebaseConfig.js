2561f07984901253a8c19259df0f7e1e503db887
zamarz <zachmarzouk@gmail.com>
2024-04-10 15:55:32 +0100

    Get Firebase authentication working

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 const analytics = getAnalytics(app);
---
+++ src/app/register/page.tsx
@@ -0,0 +1,8 @@
+import RegisterUser from "@/components/RegisterUser";
+import React from "react";
+
+const register = () => {
+  return <RegisterUser />;
+};
+
+export default register;

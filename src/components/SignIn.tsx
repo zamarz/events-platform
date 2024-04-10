eab956b5508f11f62bc46d075e0a17baa71c47df
zamarz <zachmarzouk@gmail.com>
2024-04-10 15:59:51 +0100

    Create basic sign in page

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { auth } from "../../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  return (
+    <section className="min-h-screen mx-auto container pt-8 ">
+      <form
+        onSubmit={handleSubmit}
+        className="items-center justify-center space-x-5 max-w-md mx-auto mt-5 p-4 rounded shadow bg-tertiary-alt border border-tertiary-light"
+      >
+        <h2 className="mb-4 text-xl font-semibold">Sign In</h2>
+        <div className="mb-4">
+          <label htmlFor="email" className="block font-medium mb-2">
+            Email:
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="w-full border border-gray-300 rounded py-2 px-3"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block font-medium mb-2">
+            Password:
+          </label>
+          <input
+            type="password"
+            placeholder="Enter a password"
+            id="password"
+            name="password"
+            className="w-full border border-gray-300 rounded py-7 my-2 px-4"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button className="button" type="submit">
+          Sign In
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default SignIn;

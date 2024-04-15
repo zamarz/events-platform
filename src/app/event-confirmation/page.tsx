8b4dd02158f978930e802d252cfcd18422767f51
zamarz <zachmarzouk@gmail.com>
2024-04-15 10:35:18 +0100

    Add event confirmation placeholder

---
+++ src/app/event-confirmation/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EventConfirmation = () => {
+  return (
+    <div>
+      <div>
+        <h2>
+          Great, you're all signed up for EVENT NAME which begins on DATE!
+        </h2>
+        <button className="button">Add to your Google Calendar</button>
+      </div>
+    </div>
+  );
+};
+
+export default EventConfirmation;

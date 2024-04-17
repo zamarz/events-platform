763b0310332700248d8e63525be9e26d985cfc60
zamarz <zachmarzouk@gmail.com>
2024-04-17 09:37:24 +0100

    Add images to event card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Image from "next/image";
 import Link from "next/link";
 
 const EventCard = ({ eventInfo }: any) => {
@@ -7,6 +10,23 @@ const EventCard = ({ eventInfo }: any) => {
   return (
     <div className="mx-4 border border-primary py-3">
       <div className="flex items-center flex-wrap justify-start mx-5 px-6 py-3">
+        {eventInfo.logo ? (
+          <Image
+            src={`${eventInfo.logo.url}`}
+            alt="An image of a person running"
+            width={200}
+            height={200}
+            layout="responsive"
+          />
+        ) : (
+          <Image
+            src={"/assets/running-person-placeholder.jpg"}
+            alt="An image of a person running on a road"
+            width={200}
+            height={200}
+            layout="responsive"
+          />
+        )}
         <h2 className="text-l font-semibold">{eventInfo.name.text}</h2>
         <p className="text-pretty">{eventInfo.summary}</p>
         <p className="px-2">{eventInfo.start.local}</p>

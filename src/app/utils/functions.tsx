16beac59c0cac6a28998531b52f01581da3d6610
zamarz <zachmarzouk@gmail.com>
2024-04-17 10:10:56 +0100

    Add event date getters to function file

--- src/app/utils/functions.tsx
+++ src/app/utils/functions.tsx
@@ -1,5 +1,6 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../../firebaseConfig";
+import { format, parseISO } from "date-fns";
 
 export const writeNewUserInfoToDB = async (user: any) => {
   try {
@@ -12,4 +13,18 @@ export const writeNewUserInfoToDB = async (user: any) => {
   } catch (error) {
     console.error(error);
   }
+};
+
+export const eventStartDateGetter = (data: any, setEventStartDate: any) => {
+  const dateString = data.start.local;
+  const dateObject = parseISO(dateString);
+  const formattedStartDate = format(dateObject, "MMMM dd, yyyy h:mm aa");
+  setEventStartDate(formattedStartDate);
+};
+
+export const eventEndDateGetter = (data: any, setEventEndDate: any) => {
+  const dateString = data.end.local;
+  const dateObject = parseISO(dateString);
+  const formattedEndDate = format(dateObject, "MMMM dd, yyyy h:mm aa");
+  setEventEndDate(formattedEndDate);
 };
--- src/components/EventSingle.tsx
+++ src/components/EventSingle.tsx
@@ -6,24 +6,30 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import EventBriteWidget from "./EventBriteWidget";
+import {
+  eventEndDateGetter,
+  eventStartDateGetter,
+} from "@/app/utils/functions";
 
 const EventSingle = () => {
   const [eventInfo, setEventInfo] = useState(null);
+  const [eventStartDate, setEventStartDate] = useState("");
+  const [eventEndDate, setEventEndDate] = useState("");
 
   const params = useParams();
 
   useEffect(() => {
     getEventById(params.id)
       .then((data: any) => {
-        setEventInfo(data);
+        eventStartDateGetter(data, setEventStartDate);
+        eventEndDateGetter(data, setEventEndDate);
+        return setEventInfo(data);
       })
       .catch((error: Error) => {
         console.error(error);
       });
   }, []);
   console.log(eventInfo, "eventInfo on single page");
-
-  //can put a placeholder image in the below if no image included
 
   return (
     <section className="min-h-screen px-10 py-10 mx-auto container">
@@ -49,8 +55,12 @@ const EventSingle = () => {
             {eventInfo.name.text}
           </h2>
           <p className="text-pretty py-3">{eventInfo.summary}</p>
-          <p className="py-3">Start: {eventInfo.start.local}</p>
-          <p className="py-3">End: {eventInfo.end.local}</p>
+          <p className="py-3">
+            <span className="font-semibold">Start:</span> {eventStartDate}
+          </p>
+          <p className="py-3">
+            <span className="font-semibold">End:</span> {eventEndDate}
+          </p>
 
           <EventBriteWidget eventId={eventInfo.id} eventInfo={eventInfo} />
           <p className="py-3">

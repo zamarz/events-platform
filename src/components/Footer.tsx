be0cf98108617276f8f4f682fce2e725cc550cc0
zamarz <zachmarzouk@gmail.com>
2024-04-19 13:32:20 +0100

    Make footer more responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import { BsFacebook } from "react-icons/bs";
 import { MdEvent } from "react-icons/md";
 import { RiBookMarkLine } from "react-icons/ri";
 
 const Footer = () => {
   const style = { fontSize: "1.5em" };
   return (
-    <footer className="bg-primary text-white py-8 px-6 sm:px-8 lg:px-16">
+    <footer className="bg-primary text-white py-8 px-6 sm:px-8 lg:px-16 w-full">
       <div className="container mx-auto max-w-screen-lg xl:max-w-screen-xl">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <Link href="/" className="font-black text-tertiary-light">
@@ -20,7 +21,13 @@ const Footer = () => {
           <div className="space-y-2 flex items-center">
             <MdEvent style={style} />
             <h4 className="font-semibold px-3">
-              <Link href="/">My Events</Link>
+              <Link href="/events">Events</Link>
+            </h4>
+          </div>
+          <div className="space-y-2 flex items-center">
+            <BsFacebook style={style} />
+            <h4 className="font-semibold px-3">
+              <Link href="/">Follow Us</Link>
             </h4>
           </div>
         </div>

484f6db09636461826f534bb9b1004707d2f7871
zamarz <zachmarzouk@gmail.com>
2024-04-19 14:14:27 +0100

    Make HomePage more responsive

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,9 +7,11 @@ const HomePage = () => {
   return (
     <section className="min-h-screen px-10 py-10 mx-auto container">
       <HomeImage />
-      <div className="pt-7 flex justify-between w-full mx-auto py-5">
-        <h3 className="text-2xl">Find your next running event</h3>
-        <h4 className="text-xl text-primary font-semibold hover:text-tertiary-light dark:text-tertiary-light dark:hover:text-primary">
+      <div className="pt-7 flex flex-wrap md:flex-none items-center justify-between w-full mx-auto py-5">
+        <h3 className=" py-5 text-lg md:text-2xl">
+          Find your next running event
+        </h3>
+        <h4 className="text-xl text-primary underline md:no-underline  font-semibold hover:text-tertiary-light dark:text-tertiary-light dark:hover:text-primary">
           <Link href="/events">See All Events</Link>
         </h4>
       </div>

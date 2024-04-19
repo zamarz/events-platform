e6351fb35ab400abe0b78face0b1c8edec726511
zamarz <zachmarzouk@gmail.com>
2024-04-19 11:47:55 +0100

    Change main image style

--- src/components/HomeImage.tsx
+++ src/components/HomeImage.tsx
@@ -21,18 +21,23 @@ const HomeImage = () => {
               <Link href="/events">See Events</Link>
             </button>
           </div>
-          <Image
-            src={"/assets/running-group-2.jpg"}
-            alt="An image of people running in an arrow formation, taken from above."
-            width={500}
-            height={500}
-            priority={true}
-            layout="responsive"
-            className="mx-auto"
-          />
+          <div
+            style={{ position: "relative", width: "500px", height: "500px" }}
+          >
+            <Image
+              src={"/assets/running-group-2.jpg"}
+              alt="An image of people running in an arrow formation, taken from above."
+              sizes="300px"
+              priority={true}
+              fill={true}
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </div>
         </div>
       ) : (
-        <div className="flex items-center justify-center space-x-4 dark:bg-tertiary-light/90">
+        <div className="flex items-center justify-center space-x-4 bg-tertiary-alt dark:bg-tertiary-light/90">
           <div className="px-4 py-4">
             <h1 className="font-bold lg:text-2xl dark:text-white">
               Join today. Run tomorrow.
@@ -42,15 +47,20 @@ const HomeImage = () => {
             </p>
             <button className="button">Sign up</button>
           </div>
-          <Image
-            src={"/assets/running-arrow.jpg"}
-            alt="An image of people running in an arrow formation, taken from above."
-            width={500}
-            height={500}
-            priority={true}
-            layout="responsive"
-            className="mx-auto"
-          />
+          <div
+            style={{ position: "relative", width: "500px", height: "500px" }}
+          >
+            <Image
+              src={"/assets/running-arrow.jpg"}
+              alt="An image of people running in an arrow formation, taken from above."
+              sizes="300px"
+              priority={true}
+              fill={true}
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </div>
         </div>
       )}
     </div>

68babee772854e72c027425aaf48804ae7e4b37d
zamarz <zachmarzouk@gmail.com>
2024-04-19 14:18:58 +0100

    Make events page responsive

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,11 +2,13 @@ import EventsList from "@/components/EventList";
 
 const Events = () => {
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center w-full">
       <div className="text-2xl font-semibold py-7">
         <h1>All Events</h1>
       </div>
-      <EventsList />
+      <div className="w-full min-h-screen">
+        <EventsList />
+      </div>
     </section>
   );
 };

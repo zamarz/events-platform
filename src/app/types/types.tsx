faac23547a1ecc3b0197d350538152b73785750c
zamarz <zachmarzouk@gmail.com>
2024-04-22 09:43:06 +0100

    Add types to Event List

--- src/app/types/types.tsx
+++ src/app/types/types.tsx
@@ -20,3 +20,77 @@ export type EventSummary = {
     summary: string;
   };
 };
+
+export type getEventsIdType = {
+  events: Array<Event>;
+  pagination: {
+    has_more_items: boolean;
+    object_count: number;
+    page_count: number;
+    page_number: number;
+    page_size: number;
+  };
+};
+
+export type Event = {
+  capacity: number;
+  capacity_is_custom: boolean;
+  category_id: null | number | string;
+  changed: string;
+  created: string;
+  currency: string;
+  description: {
+    html: string;
+    text: string;
+  };
+  end: {
+    local: string;
+    timezone: string;
+    utc: string;
+  };
+  facebook_event_id: null | string;
+  format_id: null | number;
+  hide_end_date: boolean;
+  hide_start_date: boolean;
+  id: string;
+  inventory_type: string;
+  invite_only: boolean;
+  is_externally_ticketed: boolean;
+  is_free: boolean;
+  is_locked: boolean;
+  is_reserved_seating: boolean;
+  is_series: boolean;
+  is_series_parent: boolean;
+  listed: boolean;
+  locale: string;
+  logo: null;
+  logo_id: null | number;
+  name: {
+    html: string;
+    text: string;
+  };
+  online_event: boolean;
+  organization_id: string;
+  organizer_id: string;
+  privacy_setting: string;
+  published: string;
+  resource_uri: string;
+  shareable: boolean;
+  show_colors_in_seatmap_thumbnail: boolean;
+  show_pick_a_seat: boolean;
+  show_remaining: boolean;
+  show_seatmap_thumbnail: boolean;
+  source: string;
+  start: {
+    local: string;
+    timezone: string;
+    utc: string;
+  };
+  status: "started";
+  subcategory_id: null | string;
+  summary: string;
+  tx_time_limit: number;
+  url: string;
+  venue_id: null | string;
+  version: null | string;
+};
--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -4,15 +4,16 @@ import { getEventsId } from "@/app/utils/api";
 import { useEffect, useState } from "react";
 import EventCard from "./EventCard";
 import Loading from "@/app/loading";
+import { Event, getEventsIdType } from "@/app/types/types";
 
 const EventsList = () => {
-  const [eventsData, setEventsData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [eventsData, setEventsData] = useState<getEventsIdType | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     setLoading(true);
     getEventsId()
-      .then((data: any) => {
+      .then((data: getEventsIdType) => {
         setEventsData(data);
       })
       .catch((error: Error) => {
@@ -29,7 +30,7 @@ const EventsList = () => {
     <section className="w-full mx-auto container">
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 mx-auto gap-4 py-3">
         {eventsData ? (
-          eventsData.events.map((event: any) => {
+          eventsData.events.map((event: Event) => {
             return <EventCard key={event.id} eventInfo={event} />;
           })
         ) : (
